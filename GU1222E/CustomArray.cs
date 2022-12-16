c5abe03f1f39015cf6d15dc70304f63a08011723
LamNX <lamnguyen179@gmail.com>
2022-12-16 18:15:04 +0700

    Initial commit

---
+++ GU1222E/CustomArray.cs
@@ -0,0 +1,10 @@
+﻿namespace GU1222E;
+
+public class CustomArray
+{
+    public static string Str = "Đây là 1 biến string trong class CustomArray";
+    public static void Fn()
+    {
+        Console.WriteLine("Đây là 1 function trong class CustomArray");
+    }
+}
---
+++ GU1222E/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using GU1222E;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int Input()
+{
+    Console.Write("Nhập 1 số: ");
+    string inp = Console.ReadLine();
+    int value = Int32.Parse(inp);
+    return value;
+}
+
+void Print(int a, int b)
+{
+    Console.WriteLine($"a={a}, b={b}");
+    if (a == 10)
+    {
+        return;
+    }
+
+    Console.WriteLine("A không phải bằng 10");
+}
+
+int Plus(int a, int b)
+{
+    if (a == 10)
+    {
+        return b;
+    }
+
+    return a + b;
+}
+
+int n = 10;
+int[] arr = new int[n];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i * 2;
+}
+
+// int a = Input();
+// int b = Input();
+// int c = Plus(a, b);
+// Console.WriteLine($"Tổng của {a} và {b} là: {c}");
+// Print(a, b);
+
+Console.WriteLine(CustomArray.Str);
+CustomArray.Fn();
+Console.WriteLine(arr.Length);
+Console.WriteLine(arr[5]);
